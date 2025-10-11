23241d202d17df054cd784343cb440ed2780afbd
swetha.k <swetha_kanneganti@yahoo.co.in>
2025-10-11 17:13:07 -0400

    implemented basic routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
+import About  from './pages/About';
+import ErrorPage from './pages/ErrorPage';
+import Home from './pages/Home';
+import Header from './shared/Header';
+import Favorite from './pages/Favorite';
+import {Routes, Route} from "react-router";
 import './App.css'
 
 function App() {
   return (
     <div>
-      <h1>Final project</h1>
+      <h1>Heading</h1>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/favorite" element={<Favorite />}></Route>
+        <Route path="*" element={<ErrorPage />}></Route>
+      </Routes> 
     </div>
   )
 }
---
+++ src/pages/ErrorPage.jsx
@@ -0,0 +1,7 @@
+export default function ErrorPage(){
+    return (
+        <div>
+            <h2>This is error display page.</h2>
+        </div>
+    )
+}
